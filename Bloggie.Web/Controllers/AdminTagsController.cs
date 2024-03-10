c6b621365357a238bd59083397b732429915f76f
vedant115 <vedant.a.rahane@gmail.com>
2024-03-10 12:25:22 +0530

    Add CRUD Operations for Tags

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private BloggieDbContext _bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            _bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -11,10 +20,95 @@ namespace Bloggie.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult SubmitTag()
+        public IActionResult Add(AddTagRequest addTagRequest)
         {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
 
-            return View("Add");
+            _bloggieDbContext.Tags.Add(tag);
+            _bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            // use db context to read the tags
+            var tags = _bloggieDbContext.Tags.ToList();
+
+            return View(tags);
+        }
+
+        // input parameter should match the route name
+        [HttpGet]
+        public IActionResult Edit(Guid id) 
+        {
+            //var tag = _bloggieDbContext.Tags.Find(id);
+            // OR
+            var tag = _bloggieDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+            if (tag != null) 
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName,
+                };
+
+                return View(editTagRequest);
+            } 
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest) 
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName,
+            };
+
+            var existingTag = _bloggieDbContext.Tags.Find(tag.Id);
+
+            if(existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                //save changes
+                _bloggieDbContext.SaveChanges();
+                //show success notification
+                return RedirectToAction("Edit", new { id = editTagRequest.Id });
+            }
+
+            //show fail notification
+            return RedirectToAction("Edit", new {id = editTagRequest.Id});
+        }
+
+        [HttpPost]
+        public IActionResult Delete(EditTagRequest editTagRequest) 
+        {
+            var tag = _bloggieDbContext.Tags.Find(editTagRequest.Id);
+
+            if (tag != null)
+            {
+                _bloggieDbContext.Tags.Remove(tag);
+                _bloggieDbContext.SaveChanges();
+
+                //show success
+                return RedirectToAction("List");
+            }
+            //show error
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
         }
     }
 }
